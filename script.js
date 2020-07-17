2f295a276c7df36227c040008f4e0e446dcda3b3
Pneuma <syca0714@gmail.com>
2020-07-17 23:12:30 +0900

    .

--- script.js
+++ script.js
@@ -26,33 +26,19 @@ let groundLayer, coinLayer;
 let text;
 
 function preload() {
-    this.load.setBaseURL('http://labs.phaser.io');
-
-    this.load.image('sky', 'assets/skies/space3.png');
-    this.load.image('logo', 'assets/sprites/phaser3-logo.png');
-    this.load.image('red', 'assets/particles/red.png');
+    this.load.image('block', 'assets/block.png');
+    this.load.image('player', 'assets/player.png');
 }
 
 function create() {
-    this.add.image(400, 300, 'sky');
+    const player = this.physics.add.sprite(100, 450, 'player');
+    const block = this.add.sprite(200, 450, 'block');
 
-    var particles = this.add.particles('red');
-
-    var emitter = particles.createEmitter({
-        speed: 100,
-        scale: { start: 1, end: 0 },
-        blendMode: 'ADD'
-    });
-
-    var logo = this.physics.add.image(400, 100, 'logo');
-
-    logo.setVelocity(100, 200);
-    logo.setBounce(1, 1);
-    logo.setCollideWorldBounds(true);
-
-    emitter.startFollow(logo);
+    player.setBounce(0.2);
+    player.setCollideWorldBounds(true);
+    player.body.setGravityY(300);
+    this.physics.add.collider(player, block);
 }
 
 function update() {
-
 }
