479501fb8d8df72cf4739f2aeab6555ff540c4bf
Pneuma <syca0714@gmail.com>
2020-07-17 20:23:05 +0900

    .

--- main.js
+++ main.js
@@ -3,12 +3,10 @@ const path = require('path');
 
 const app = express();
 
-const PORT = process.env.PORT
-
 app.use('/', express.static(__dirname));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(PORT);
+app.listen(process.env.PORT);
---
+++ script.js
@@ -0,0 +1,38 @@
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: {y: 500},
+            debug: false
+        }
+    },
+    scene: {
+        key: 'main',
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+let game = new Phaser.Game(config);
+
+let map;
+let player;
+let cursors;
+let groundLayer, coinLayer;
+let text;
+
+function preload() {
+
+}
+
+function create() {
+
+}
+
+function update() {
+
+}
