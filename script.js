d3b46cb61fe3137e60e426aefb37f7a226cccd41
Pneuma <syca0714@gmail.com>
2020-07-17 23:36:11 +0900

    Update script.js

--- script.js
+++ script.js
@@ -33,8 +33,8 @@ function preload() {
 function create() {
     player = this.physics.add.sprite(100, 450, 'player');
     platforms = this.physics.add.staticGroup();
-    platforms.create(100, 100, 'block');
-    platforms.create(200, 300, 'block');
+    platforms.create(500, 100, 'block');
+    platforms.create(400, 300, 'block');
 
     player.setBounce(0.2);
     player.setCollideWorldBounds(true);
